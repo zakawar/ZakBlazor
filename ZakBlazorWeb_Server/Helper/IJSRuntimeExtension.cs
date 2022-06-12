9dbe3dd7762e3112042a32effd371c196b8d32ee
zakawar <zikri2005@gmail.com>
2022-06-11 23:40:51 -0400

    Helper and JS folders added and statement incomplet

---
+++ ZakBlazorWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+
+namespace ZakBlazorWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess( this IJSRuntime _JsRuntime, string message)
+        {
+            await _JsRuntime.InvokeVoidAsync("ShowToastr","success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime _JsRuntime, string message)
+        {
+            await _JsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+}
+
+       
+    }
+}
---
+++ ZakBlazorWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,13 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+Swal.fire=(title, text,icon)=> {
+
+}
