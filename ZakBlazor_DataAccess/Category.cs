1ff56da514f6f3e29c271e80211ac72bbc50f080
warsame <zikri2005@gmail.com>
2022-06-14 13:06:55 -0400

    Push the category table in the database

--- ZakBlazor_DataAccess/Category.cs
+++ ZakBlazor_DataAccess/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace ZakBlazor_DataAccess
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        public string? Name { get; set; }
+
+        public DateTime CreatedTime { get; set; }
     }
 }
